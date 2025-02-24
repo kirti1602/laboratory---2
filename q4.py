6605ee78b93f87c69ea5fec88563b2e2686f755c
kirti1602 <kirtikashyap9a@gmail.com>
2025-02-24 12:17:10 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,7 @@
+# Check if a character is a vowel or consonant
+char = input("Enter a character: ").lower()
+
+if char in 'aeiou':
+    print(f"{char} is a vowel.")
+else:
+    print(f"{char} is a consonant.")
